a5e662747008a59515e21b67614a60bab622855e
zeebraa00 <zeebraa00@gmail.com>
2021-07-05 01:18:35 +0900

    problem40 backtracking

---
+++ 40_boj_15650.py
@@ -0,0 +1,23 @@
+n, m = map(int, input().split())
+
+visited = [0 for i in range(8)]
+path = [0 for i in range(8)]
+
+def backtracking(cnt) :
+    if cnt == m :
+        for i in range(m) :
+            print(path[i], end=" ")
+        print()
+        return
+
+    for i in range(n) :
+        if visited[i] == 0 :
+            if path[cnt-1] > i+1 :
+                continue
+            path[cnt] = i+1
+            visited[i] = 1
+            backtracking(cnt+1)
+            visited[i] = 0
+
+
+backtracking(0)
