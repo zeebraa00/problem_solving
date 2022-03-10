9e11a8cc1c8757c5f3ec06312bb03d838a231ace
zeebraa00 <zeebraa00@gmail.com>
2022-03-10 09:05:12 +0900

    problem63 recursion

---
+++ 63_boj_10870.py
@@ -0,0 +1,11 @@
+import sys
+
+def fibo(n) :
+    if n == 0 :
+        return 0
+    elif n == 1 :
+        return 1
+    return fibo(n-1) + fibo(n-2)
+
+n = int(sys.stdin.readline())
+print(fibo(n))
